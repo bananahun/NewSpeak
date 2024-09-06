e4892a6f9a5ad2891cc898c567500c29f4854d12
최지우 <andyandy0409@naver.com>
2024-09-06 13:45:15 +0900

    [#4] feat: 다크모드

--- NewSpeak/src/components/Nav/Nav.tsx
+++ NewSpeak/src/components/Nav/Nav.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/NewSpeak.png';
+import logoWhite from '../../assets/NewSpeakWhite.png';
 import styles from './Nav.module.scss';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 
 const Nav: React.FC = () => {
+  const [mainLogo, setMainLogo] = useState(logo);
+  const [selectedTheme, setSelectedTheme] = useState(
+    localStorage.getItem('theme') || 'light',
+  );
+
+  useEffect(() => {
+    setSelectedTheme(localStorage.getItem('theme') || 'light');
+  }, [localStorage.getItem('theme')]);
+
+  useEffect(() => {
+    if (selectedTheme === 'light') {
+      setMainLogo(logo);
+    } else {
+      setMainLogo(logoWhite);
+    }
+  });
+
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
-          <img src={logo} width={'120px'} />
+          <img src={mainLogo} width={'160px'} />
+        </div>
+        <div className={styles.switcher}>
+          <ThemeSwitcher />
         </div>
       </nav>
     </>
