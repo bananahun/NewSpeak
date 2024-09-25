ec4b0517588a26f5d606781b21a7fd8f0e1c629e
최지우 <andyandy0409@naver.com>
2024-09-25 16:22:10 +0900

    react-speech-kit 제거

--- NewSpeak/src/components/Modal/ConversationModal.tsx
+++ NewSpeak/src/components/Modal/ConversationModal.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useSpeechRecognition } from 'react-speech-kit';
 
 const ConversationModal = () => {
   const [value, setValue] = useState('');
