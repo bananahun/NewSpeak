9436cad07445c92f0196f26448eaeba702e53bce
박영훈 <pyh007264@gmail.com>
2024-09-26 20:57:14 +0900

    api permitAll Update SecurityConfig.java

--- backend/src/main/java/com/ssafy/newspeak/security/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/newspeak/security/config/SecurityConfig.java
@@ -95,7 +95,7 @@ public class SecurityConfig {
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico", "/h2-console/**").permitAll()
                         .requestMatchers("/api/v1/auth/signUp","/api/v1/auth/email").hasRole("GUEST")
-                        .requestMatchers("/api/**").hasRole("USER")
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(oAuth2LoginSuccessHandler)
