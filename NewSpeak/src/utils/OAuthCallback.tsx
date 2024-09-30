c5051bd7bbf0be700eec98797f772b1d5103b5f4
최지우 <andyandy0409@naver.com>
2024-09-30 11:02:58 +0900

    [#63] feat: isLoggedIn 수정

--- NewSpeak/src/utils/OAuthCallback.tsx
+++ NewSpeak/src/utils/OAuthCallback.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useAuthStore from '../store/AuthStore';
 
 const OAuthCallback = () => {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAuthStore();
 
   useEffect(() => {
-    navigate('/register');
+    if (isLoggedIn) {
+      navigate('/');
+    } else {
+      navigate('/register');
+    }
   }, [navigate]);
 
   return <div>Loading</div>;
