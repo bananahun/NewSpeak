d22f2530370147ecfadc008da29dad5099bcc934
김동환 <ehdghks9719@naver.com>
2024-10-07 15:35:28 +0900

    [#97] feat: redis 설정

--- backend/src/main/java/com/ssafy/newspeak/config/RedisConfig.java
+++ backend/src/main/java/com/ssafy/newspeak/config/RedisConfig.java
@@ -1,23 +1,31 @@
 package com.ssafy.newspeak.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-        @Bean
-        LettuceConnectionFactory connectionFactory() {
-            return new LettuceConnectionFactory();
-        }
+    @Value("${spring.data.redis.host}")
+    private String host;
 
-        @Bean
-        public RedisTemplate<String, Object> redisTemplate() {
-            RedisTemplate<String, Object> template = new RedisTemplate<>();
-            template.setConnectionFactory(connectionFactory());
-            return template;
-        }
+    @Value("${spring.data.redis.port}")
+    private int port;
 
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    //RedisTemplate 사용을 위한 추가
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
