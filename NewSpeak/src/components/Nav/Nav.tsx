1c2143e69a1bce0c24f19bba819ac2b71137bfe9
정훈 <vision981111@naver.com>
2024-10-08 09:01:58 +0900

    Fe 96 fix css수정

--- NewSpeak/src/components/Nav/Nav.tsx
+++ NewSpeak/src/components/Nav/Nav.tsx
@@ -1,39 +1,27 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import InfoIcon from '@mui/icons-material/Info';
-import ArticleIcon from '@mui/icons-material/Article';
-import DescriptionIcon from '@mui/icons-material/Description';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import styles from './Nav.module.scss'; // SCSS 스타일 임포트
+import React from "react";
+import { NavLink } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+import ArticleIcon from "@mui/icons-material/Article";
+import DescriptionIcon from "@mui/icons-material/Description";
+import styles from "./Nav.module.scss"; // SCSS 스타일 임포트
 
 interface NavProps {
   open: boolean;
-  handleDrawerToggle: () => void;
 }
 
-const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
+const Nav: React.FC<NavProps> = ({ open }) => {
   return (
     <div className={styles.navContainer}>
-      {/* 메뉴바 토글 버튼 */}
-      <IconButton onClick={handleDrawerToggle} className={styles.menuButton}>
-        {open ? <ChevronLeftIcon /> : <MenuIcon />}
-      </IconButton>
-
-      <Divider />
-
-      {/* 네비게이션 메뉴 */}
       <List className={styles.list}>
-        <NavLink to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+        ///home ///home ///home
+        <NavLink to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <ListItem
             disablePadding
             className={`${styles.listItem} ${
@@ -50,10 +38,10 @@ const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
             </ListItemButton>
           </ListItem>
         </NavLink>
-
+        ///about ///about ///about
         <NavLink
           to="/mypage"
-          style={{ textDecoration: 'none', color: 'inherit' }}
+          style={{ textDecoration: "none", color: "inherit" }}
         >
           <ListItem
             disablePadding
@@ -74,10 +62,10 @@ const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
             </ListItemButton>
           </ListItem>
         </NavLink>
-
+        ///HotArticleList ///HotArticleList ///HotArticleList
         <NavLink
           to="/about"
-          style={{ textDecoration: 'none', color: 'inherit' }}
+          style={{ textDecoration: "none", color: "inherit" }}
         >
           <ListItem
             disablePadding
@@ -87,7 +75,7 @@ const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
           >
             <ListItemButton className={styles.listItemButton}>
               <ListItemIcon className={styles.icon}>
-                <InfoIcon />
+                <LightbulbIcon />
               </ListItemIcon>
               {open && (
                 <ListItemText primary="About" className={styles.listItemText} />
@@ -95,10 +83,10 @@ const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
             </ListItemButton>
           </ListItem>
         </NavLink>
-
+        ///favoriteArticleList ///favoriteArticleList ///favoriteArticleList
         <NavLink
           to="/article"
-          style={{ textDecoration: 'none', color: 'inherit' }}
+          style={{ textDecoration: "none", color: "inherit" }}
         >
           <ListItem
             disablePadding
@@ -119,10 +107,106 @@ const Nav: React.FC<NavProps> = ({ open, handleDrawerToggle }) => {
             </ListItemButton>
           </ListItem>
         </NavLink>
-
+        ///favoriteArticleList ///favoriteArticleList ///favoriteArticleList
         <NavLink
           to="/conversation"
-          style={{ textDecoration: 'none', color: 'inherit' }}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem
+            disablePadding
+            className={`${styles.listItem} ${
+              open ? styles.listItemOpen : styles.listItemClosed
+            }`}
+          >
+            <ListItemButton className={styles.listItemButton}>
+              <ListItemIcon className={styles.icon}>
+                <DescriptionIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText
+                  primary="Conversation"
+                  className={styles.listItemText}
+                />
+              )}
+            </ListItemButton>
+          </ListItem>
+        </NavLink>
+        ///CategoryArticles ///CategoryArticles ///CategoryArticles
+        <NavLink
+          to="/conversation"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem
+            disablePadding
+            className={`${styles.listItem} ${
+              open ? styles.listItemOpen : styles.listItemClosed
+            }`}
+          >
+            <ListItemButton className={styles.listItemButton}>
+              <ListItemIcon className={styles.icon}>
+                <DescriptionIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText
+                  primary="Conversation"
+                  className={styles.listItemText}
+                />
+              )}
+            </ListItemButton>
+          </ListItem>
+        </NavLink>
+        ///WordSearch ///WordSearch ///WordSearch
+        <NavLink
+          to="/conversation"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem
+            disablePadding
+            className={`${styles.listItem} ${
+              open ? styles.listItemOpen : styles.listItemClosed
+            }`}
+          >
+            <ListItemButton className={styles.listItemButton}>
+              <ListItemIcon className={styles.icon}>
+                <DescriptionIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText
+                  primary="Conversation"
+                  className={styles.listItemText}
+                />
+              )}
+            </ListItemButton>
+          </ListItem>
+        </NavLink>
+        ///ArticleSearch ///ArticleSearch ///ArticleSearch
+        <NavLink
+          to="/conversation"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem
+            disablePadding
+            className={`${styles.listItem} ${
+              open ? styles.listItemOpen : styles.listItemClosed
+            }`}
+          >
+            <ListItemButton className={styles.listItemButton}>
+              <ListItemIcon className={styles.icon}>
+                <DescriptionIcon />
+              </ListItemIcon>
+              {open && (
+                <ListItemText
+                  primary="Conversation"
+                  className={styles.listItemText}
+                />
+              )}
+            </ListItemButton>
+          </ListItem>
+        </NavLink>
+        ///ArticleSearch ///ArticleSearch ///ArticleSearch
+        <NavLink
+          to="/conversation"
+          style={{ textDecoration: "none", color: "inherit" }}
         >
           <ListItem
             disablePadding
--- NewSpeak/src/pages/Home/Layout.tsx
+++ NewSpeak/src/pages/Home/Layout.tsx
@@ -1,159 +1,172 @@
-// import React, { useState } from 'react';
-// import { Outlet } from 'react-router-dom';
-// import { styled } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import AppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
-// import Drawer from '@mui/material/Drawer';
-// import Typography from '@mui/material/Typography';
-// import Nav from '../../components/Nav/Nav';
-// import './Layout.module.scss';
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import { createTheme } from "@mui/material/styles";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import type { Router } from "@toolpad/core";
+import Home from "@mui/icons-material/Home";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import ArticleIcon from "@mui/icons-material/Article";
+import CategoryIcon from "@mui/icons-material/Category";
+import SearchIcon from "@mui/icons-material/Search";
+import FindInPageIcon from "@mui/icons-material/FindInPage";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+import WordSearchModal from "../../components/Modal/WordSearchModal";
 
-// const drawerWidth = 240;
+// 커스텀 Navigation 타입 정의
+interface CustomNavigationItem {
+  segment?: string;
+  title: string;
+  icon: React.ReactNode;
+  onClick?: () => void;
+}
 
-// const Main = styled('main', { shouldForwardProp: prop => prop !== 'open' })<{
-//   open?: boolean;
-// }>(({ theme, open }) => ({
-//   flexGrow: 1,
-//   padding: theme.spacing(3),
-//   transition: theme.transitions.create('margin', {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   marginLeft: `-${drawerWidth}px`,
-//   ...(open && {
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     marginLeft: 0,
-//   }),
-// }));
-
-// const Layout = () => {
-//   const [open, setOpen] = useState(false);
-
-//   const handleDrawerToggle = () => {
-//     setOpen(!open);
-//   };
-
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CssBaseline />
-//       <AppBar position="fixed">
-//         <Toolbar>
-//           <Typography variant="h6" noWrap>
-//             NewsSpeak Dashboard
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer
-//         sx={{
-//           width: open ? drawerWidth : 60,
-//           flexShrink: 0,
-//           '& .MuiDrawer-paper': {
-//             width: open ? drawerWidth : 60,
-//             boxSizing: 'border-box',
-//             overflowX: 'hidden',
-//           },
-//         }}
-//         variant="permanent"
-//         anchor="left"
-//         open={open}
-//       >
-//         <Nav open={open} handleDrawerToggle={handleDrawerToggle} />
-//       </Drawer>
-//       <Main open={open}>
-//         <Toolbar />
-//         <Outlet />
-//       </Main>
-//     </Box>
-//   );
-// };
-
-// export default Layout;
-
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { Outlet } from 'react-router-dom';
-import Nav from '../../components/Nav/Nav'; // 네비게이션 컴포넌트
-import type { Router } from '@toolpad/core';
-import { createTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Home from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import styles from './Layout.module.scss'; // SCSS 파일 불러오기
-
-const theme = createTheme({
-  cssVariables: {
-    colorSchemeSelector: 'data-toolpad-color-scheme',
+// 네비게이션 메뉴 아이템 설정
+const NAVIGATION: CustomNavigationItem[] = [
+  { segment: "", title: "Home", icon: <Home /> },
+  { segment: "about", title: "About Us", icon: <LightbulbIcon /> },
+  {
+    segment: "articlelist/keyword",
+    title: "Hot Articles",
+    icon: <WhatshotIcon />,
   },
-  colorSchemes: { light: true, dark: true },
+  {
+    segment: "preferred",
+    title: "Favorite Articles",
+    icon: <ArticleIcon />,
+  },
+  {
+    segment: "articlelist",
+    title: "Category Articles",
+    icon: <CategoryIcon />,
+  },
+  { segment: "wordSearch", title: "Word Search", icon: <SearchIcon /> },
+  {
+    segment: "articlesearch",
+    title: "Article Search",
+    icon: <FindInPageIcon />,
+  },
+  { segment: "scraplist", title: "Scrap", icon: <BookmarksIcon /> },
+];
+
+// 테마 설정
+const customTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "data-toolpad-color-scheme",
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        background: {
+          default: "#F9F9FE",
+          paper: "#EEEEF9",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        background: {
+          default: "#2A4364",
+          paper: "#112E4D",
+        },
+      },
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 1024,
+      lg: 1440,
+      xl: 1536,
+    },
+  },
 });
 
-const Layout = () => {
-  const [pathname, setPathname] = useState('/home');
-  const [open, setOpen] = useState(false);
+function Layout({ pathname }: { pathname: string }) {
+  const [isWordSearchModalOpen, setIsWordSearchModalOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setOpen(!open);
+  // WordSearch 아이콘 클릭 핸들러
+  const handleWordSearchIconClick = () => {
+    setIsWordSearchModalOpen(true);
   };
 
-  const router: Router = React.useMemo(() => {
+  // WordSearchModal 닫기 핸들러
+  const handleCloseWordSearchModal = () => {
+    setIsWordSearchModalOpen(false);
+  };
+
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      {/* WordSearchModal 모달 컴포넌트 */}
+      <WordSearchModal
+        open={isWordSearchModalOpen}
+        onClose={handleCloseWordSearchModal}
+      />
+      <Outlet />
+    </Box>
+  );
+}
+
+export default function AppProviderTheme() {
+  const navigate = useNavigate();
+  const [pathname, setPathname] = React.useState("/");
+
+  const router = React.useMemo<Router>(() => {
     return {
       pathname,
       searchParams: new URLSearchParams(),
-      navigate: path => setPathname(String(path)),
+      navigate: (path) => {
+        setPathname(String(path));
+        navigate(path);
+      },
     };
   }, [pathname]);
+
+  // WordSearch 아이콘 클릭 핸들러
+  const [isWordSearchModalOpen, setIsWordSearchModalOpen] =
+    React.useState(false);
+  const handleWordSearchIconClick = () => {
+    setIsWordSearchModalOpen(true);
+  };
+
+  // `navigation`에서 Word Search 클릭 시 `handleWordSearchIconClick`만 실행하도록 수정
+  const customizedNavigation = NAVIGATION.map((navItem) => {
+    if (navItem.segment === "wordSearch") {
+      return {
+        ...navItem,
+        onClick: (event: React.MouseEvent) => {
+          event.preventDefault();
+          handleWordSearchIconClick(); // WordSearch 모달 열기
+        },
+      };
+    }
+    return navItem;
+  });
 
   return (
     <AppProvider
       branding={{
         logo: null,
-        title: 'title',
+        title: "NewSpeak",
       }}
-      navigation={[
-        { segment: 'home', title: 'Home', icon: <Home /> },
-        { segment: 'about', title: 'About Us', icon: <InfoIcon /> },
-      ]}
+      navigation={customizedNavigation}
       router={router}
-      theme={theme}
+      theme={customTheme}
     >
-      <DashboardLayout
-        sidebar={<Nav open={open} />} // Nav 컴포넌트에서 열림 상태를 관리
-        header={
-          <AppBar position="fixed" open={open} className={styles.appBar}>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label={open ? 'Close drawer' : 'Open drawer'}
-                edge="start"
-                onClick={handleDrawerToggle}
-              >
-                {open ? <ChevronLeftIcon /> : <MenuIcon />}
-              </IconButton>
-              <Typography variant="h6" noWrap component="div">
-                NewsSpeak Dashboard {/* 기존의 "Toolpad" 텍스트를 변경 */}
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        }
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'column', mt: 8 }}>
-          <Outlet />
-        </Box>
+      <DashboardLayout>
+        <Layout pathname={pathname} />
       </DashboardLayout>
     </AppProvider>
   );
-};
-
-export default Layout;
+}
