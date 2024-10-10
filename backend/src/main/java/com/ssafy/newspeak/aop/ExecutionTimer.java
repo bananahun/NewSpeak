43062118d64de7f2809a4c01447c2985e5a826c2
김동환 <ehdghks9719@naver.com>
2024-10-10 16:21:13 +0900

    aop 수정

--- backend/src/main/java/com/ssafy/newspeak/aop/ExecutionTimer.java
+++ backend/src/main/java/com/ssafy/newspeak/aop/ExecutionTimer.java
@@ -1,36 +1,37 @@
-//package com.ssafy.newspeak.aop;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.commons.lang3.time.StopWatch;
-//import org.aspectj.lang.ProceedingJoinPoint;
-//import org.aspectj.lang.annotation.Around;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.aspectj.lang.reflect.MethodSignature;
-//import org.springframework.stereotype.Component;
-//
-//@Slf4j
-//@Aspect
-//@Component
-//public class ExecutionTimer {
-//
-//    @Pointcut("execution(* com.ssafy.newspeak..controller..*.*(..))")
-//    private void timer(){}
-//
-//    @Around("timer()")
-//    public void AssumeExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-//
-//        StopWatch stopWatch = new StopWatch();
-//
-//        stopWatch.start();
-//        joinPoint.proceed();
-//        stopWatch.stop();
-//
-//        long totalTime = stopWatch.getTime();
-//
-//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-//        String methodName = methodSignature.getMethod().getName();
-//
-//        log.info("실행 메서드: {}, 실행시간: {}ms", methodName, totalTime);
-//    }
-//}
+package com.ssafy.newspeak.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class ExecutionTimer {
+
+    @Pointcut("execution(* com.ssafy.newspeak..controller..*.*(..))")
+    private void timer(){}
+
+    @Around("timer()")
+    public Object AssumeExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        Object result = joinPoint.proceed();
+        stopWatch.stop();
+
+        long totalTime = stopWatch.getTime();
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String methodName = methodSignature.getMethod().getName();
+
+        log.info("실행 메서드: {}, 실행시간: {}ms", methodName, totalTime);
+        return result;
+    }
+}
